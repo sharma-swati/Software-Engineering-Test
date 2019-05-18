46e961a42bd95abc2fbab6fa43969025018b01dc
sharma-swati <sswati6994@gmail.com>
2019-05-17 21:30:04 -0700

    Added lies for testing

--- list.java
+++ list.java
@@ -4,6 +4,10 @@ public class HelloWorld
 	public static void main(String[] args) {
 		List<Integer> list = new ArrayList<Integer>();
 		list.add(10);
+		for(int i=0;i<9;i++){
+			list.add(i);
+		}
 		int firstElement = list[0];
+		System.out.println("Output is "+firstElement);
 	}
 }
