b48cc2b74ad85ed483c1d61f5709b0d1c82a220d
sharma-swati <sswati6994@gmail.com>
2019-05-17 21:12:26 -0700

    Changed file name

---
+++ list.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class HelloWorld
+{
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(10);
+		int firstElement = list.get(0);
+	}
+}
